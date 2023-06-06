dfce750379d877f2340f6cd914459f3035d4bf3e
Vinícius Souza <viniciussouzasantos2004@gmail.com>
2023-06-06 15:21:32 -0300

    New changes

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,10 @@ const app = express()
 
 app.get('/', createCourse);
 
+app.post("/courses", (request, response) => {
+    const { name } = request.body
+    return response.json({ name })
+})
+
 
 app.listen(3333)
